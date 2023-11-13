b7c180ba4477d41c9e2322692f3342792ebf2c84
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2023-11-13 14:16:03 +0530

    initial commit

---
+++ 00_operators.py
@@ -0,0 +1,14 @@
+print('bye')
+# print('bye') 
+print(7/3) #2.3333
+print(3/7) #0.4285
+print(7+3) #10
+print(7-3) #4
+print(7*3) #21
+print(7%3) #1
+print(3-7) #-4
+print(7//3) #2 
+print(7**3) #343
+
+
+
