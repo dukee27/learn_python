3a0712199e854c2e2776debb3a43f45d2fd6724e
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2023-11-13 15:03:14 +0530

    updated

--- 01_print.py
+++ 01_print.py
@@ -1,4 +1,6 @@
 print("dukee's")
 print('dukee"s')
 print("dukee's home name is \"dog land\"") # \" to include double quotes
-print(10*"dukee's\n") 
+print(10*"dukee's\n") #print 10 times and \n for new line 
+print("dukee\\n")
+print(r"dukee\n")
