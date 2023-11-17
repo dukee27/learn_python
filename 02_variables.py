7e591dd09119dd561cd431161682013e4c233eee
Dhruv Kachhawaha <dhruv.kachhawaha2708@gmail.com>
2023-11-18 00:16:55 +0530

    new file

---
+++ 02_variables.py
@@ -0,0 +1,85 @@
+y = 'a'
+x = 9
+z = True
+u = '1'
+print(type(y))
+print(type(x))
+print(type(z))
+print("type of u: " , type(u))
+print(z)
+print(int(z))
+# print(int(y)) 
+d = 9.1
+print(type(d))
+
+# =====================================
+# Slicing 
+name = "dukee"
+print(name[0])
+print(name[-1])
+print(name[0:2])# du
+print(name[1:4]) # uke
+print(name[1:]) # ukee
+print(name[:4]) #duke
+print(len(name))
+print(len("dukee"))
+
+#=========================================
+# note: strings in python are im-mutable
+rollNo = [1,2,3,4,5]
+print(rollNo)
+rollNo[2]=5 
+print(rollNo)
+name1 = "robin"
+name1[2]= 'o'
+print(name1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
